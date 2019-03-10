1c495a1ad780ccdc983e2000edbcd0bcbfc162a8
Jens Kerrinnes <jens.kerrinnes@gmail.com>
2019-03-10 20:13:49 +0100

    Firs commit

---
+++ include/mincon.h
@@ -0,0 +1,20 @@
+#define MIN_ID_WD       10
+#define MIN_ID_MIDI     20
+#define MIN_ID_TERM     0
+#define MIN_ID_RESET    11
+#define MIN_ID_COMMAND  12
+#define MIN_ID_SOCKET   13
+    
+#define COMMAND_IP          1
+#define COMMAND_GW          2
+#define COMMAND_MAC         3
+#define COMMAND_SSID        4
+#define COMMAND_PASSWD      5
+#define COMMAND_INFO        6
+#define COMMAND_ETH_STATE   7
+
+#define SOCKET_DISCONNECTED  0
+#define SOCKET_CONNECTED     1
+
+#define ETH_INFO_ETH  0
+#define ETH_INFO_WIFI 1
